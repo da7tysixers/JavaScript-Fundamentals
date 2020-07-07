97428e4895355c978864c78372b301afbb9ebe46
Emmanuel Orubele <blontf@gmail.com>
2020-07-07 10:52:49 -0400

    changes to js files added event

--- js/variable.js
+++ js/variable.js
@@ -1,4 +1,34 @@
 // create a variable and call it and print it on html page.
 
-var carName = "Volvo"; // creates and initializes a variable named carName.
+var carName = "Music"; // creates and initializes a variable named carName.
 document.getElementById("demo").innerHTML = carName;
+
+//javascript creating a method that has lots of parameter.
+//a person object
+var person = {
+    firstName: "Emmanuel",
+    lastName: "Orubele",
+    age: 22,
+    changeName: function(lastName) {
+        this.lastName = lastName;
+    }
+};
+
+
+console.log(person.firstName);
+console.log(person.age);
+
+person.changeName('john');
+console.log(person.lastName);
+
+
+/*
+    types of click event/ JS event
+    onclick
+    onchange
+    onmouseover
+    onmouseout
+    onkeydown
+    onload
+
+*/
