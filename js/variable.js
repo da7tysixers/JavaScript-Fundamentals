2df4a69019bf6044cf7a0d23f7c331b7c37f4ded
Emmanuel Orubele <blontf@gmail.com>
2020-07-06 22:50:04 -0400

    changes to .js files

---
+++ js/variable.js
@@ -0,0 +1,4 @@
+// create a variable and call it and print it on html page.
+
+var carName = "Volvo"; // creates and initializes a variable named carName.
+document.getElementById("demo").innerHTML = carName;
